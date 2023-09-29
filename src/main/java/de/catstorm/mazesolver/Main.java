6b0a6454dd7055446b18e7c66e290629ea4f2f96
Cat <contact@catstorn.de>
2023-09-29 22:59:12 +0200

    Made main part of functional loop (UNTESTED!)

--- src/main/java/de/catstorm/mazesolver/Main.java
+++ src/main/java/de/catstorm/mazesolver/Main.java
@@ -1,7 +1,6 @@
 package de.catstorm.mazesolver;
 
 import java.io.IOException;
-import java.util.EmptyStackException;
 import java.util.Stack;
 
 //For anyone reading this who isn't me: Have fun with the completely undocumented code
@@ -11,7 +10,7 @@ public class Main
 
     public static void main(String[] args) throws IOException
     {
-        Parser.parse(args[0]);
+        Parser.parse("C:/Users/anton/IdeaProjects/Mzesolver/src/main/resources/yes.hex");
         returnStack.push(new NodeWithNumerator((byte) -16));
         loop();
     }
@@ -20,18 +19,27 @@ public class Main
     private static void loop()
     {
         var top = returnStack.peek();
-        if(top.getMaxNumerator() < top.getNumerator())
+        while (top.getMaxNumerator() > top.getNumerator())
         {
-            if (returnStack.size())
+            if (top.id == -16)
+            {
+                if (returnStack.size() == StoredNodes.INSTANCE.getAllNodes().size() + 1)
+                {
+                    System.out.println("Nice");
+                }
+            }
+            else if (top.getMaxNumerator() <= top.getNumerator())
+            {
+                var next = top.next();
+                for (var node : returnStack.stream().toList())
+                {
+                    if (next.id == node.id) return;
+                }
+                returnStack.push(new NodeWithNumerator(next.id));
+                System.out.println("69");
+                loop();
+            }
         }
-        else if (top.id == -16)
-        {
-            //TODO: return to home
-        }
-        else if (top.getMaxNumerator() >= top.getNumerator())
-        {
-            returnStack.push(new NodeWithNumerator(top.next().id));
-            loop();
-        }
+        returnStack.pop();
     }
 }
